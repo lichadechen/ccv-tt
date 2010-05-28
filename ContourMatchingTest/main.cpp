7b962e166ec4b0e5dc32ac537137bb642109c570
amit.getinfo <amit.getinfo@85fc5e24-5cb5-a459-a2e3-60d0933772b6>
2010-05-28 09:52:44 +0000

    Committing Contour matching test

---
+++ ContourMatchingTest/main.cpp
@@ -0,0 +1,29 @@
+// ContourMatchingTest Project
+
+#include "stdafx.h"
+
+#include "cv.h"
+#include "highgui.h"
+
+int main();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return main();
+}
+
+int main()
+{
+IplImage* img = cvLoadImage("main.jpg",0);
+IplImage* temp = cvLoadImage("template.jpg",0);
+IplImage* contourDrawn = cvCloneImage(img);
+
+cvNamedWindow("Main Image");
+
+
+cvNamedWindow("Template Image");
+
+
+return 0;
+
+}
